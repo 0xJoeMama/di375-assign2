764773c90bef48541412a196ad2585f5ddae7dbf
0xJoeMama <0xjoemama@gmail.com>
2024-12-21 20:35:16 +0200

    feat: karnaugh(wrong) maps for part 2

--- code/part-1/prompt.vhdl
+++ code/part-1/prompt.vhdl
@@ -1,6 +1,6 @@
 U1: process (A, B, C, D) is 
 begin
-  If ((B = '1' and C = D) then
+  if ((B = '1' and C = D) then
     Y <= '1';
   elsif ((A = '0' and A /= C) then
     Y <= '1';
@@ -9,9 +9,9 @@ begin
   end if;
 end process;
 
-U2:process (A, B, C, D) is 
+U2: process (A, B, C, D) is 
 begin
-  If ((D = '1' and B = C) then
+  if ((D = '1' and B = C) then
     X <= '1';
   else
     X <= '0';
